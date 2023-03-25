d4682a11666394cc0455f6d7e1cf21c914619607
ThandarSwe24 <thandarswe997@gmail.com>
2023-03-25 12:58:24 +0630

    redux

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-
+import { Provider } from 'react-redux'
 import { configureStore } from './store/store'
 
-const store = configureStore().store;
+const  store= configureStore().store;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
